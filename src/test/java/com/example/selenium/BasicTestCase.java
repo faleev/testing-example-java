2b6bc75911f1894211fe3e3a70b49e605612a097
faleev <pavel.faleev@gmail.com>
2013-12-05 16:22:30 +0200

    Adding ability to choose which driver have to be run.

--- src/test/java/com/example/selenium/BasicTestCase.java
+++ src/test/java/com/example/selenium/BasicTestCase.java
@@ -1,21 +1,48 @@
 package com.example.selenium;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BasicTestCase {
 
     protected WebDriver driver;
 
-    @BeforeClass
-    public void initializeWebDriver() throws Exception {
+    private void startFirefoxDriver() {
         driver = new FirefoxDriver();
     }
 
+    private void startChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @BeforeClass
+    @Parameters({"browser"})
+    public void initializeWebDriver(@Optional("chrome") String browser) throws Exception {
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                startFirefoxDriver();
+                break;
+
+            case "chrome":
+                startChromeDriver();
+                break;
+
+            default:
+                startFirefoxDriver();
+                break;
+        }
+    }
+
+
+
     @AfterClass(alwaysRun=true)
-    public void tearDown() throws Exception {
+    public void quitWebDriver() throws Exception {
         if (driver != null) driver.quit();
     }
 }
