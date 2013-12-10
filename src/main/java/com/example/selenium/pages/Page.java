e1438dbb489c8813fdd026d198802bb5fb73db28
faleev <pavel.faleev@gmail.com>
2013-12-10 15:34:42 +0200

    Explicit wait method added.

--- src/main/java/com/example/selenium/pages/Page.java
+++ src/main/java/com/example/selenium/pages/Page.java
@@ -1,6 +1,9 @@
 package com.example.selenium.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class Page {
 
@@ -11,8 +14,22 @@ public abstract class Page {
         this.driver = driver;
     }
 
+    public String getPageURL() {
+        return pageURL;
+    }
+
     public void openPage() {
         driver.get(pageURL);
+    }
+
+    public void waitForElement(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitForElement(WebElement element, int timeout) {
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 
 }
