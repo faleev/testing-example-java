a2c0c34ed2ffd859c4119d8324aff38f31a63ecb
faleev <pavel.faleev@gmail.com>
2013-12-10 12:42:04 +0200

    Changing page's inheritance strategy, adding main page's URL to the constructor.

--- src/main/java/com/example/selenium/pages/wikipedia/WikipediaMainPage.java
+++ src/main/java/com/example/selenium/pages/wikipedia/WikipediaMainPage.java
@@ -1,36 +1,21 @@
 package com.example.selenium.pages.wikipedia;
 
-import com.example.selenium.pages.Page;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class WikipediaMainPage extends Page {
+public class WikipediaMainPage extends WikipediaCommonPage {
 
     public WikipediaMainPage(WebDriver driver) {
         super(driver);
+        super.pageURL = "http://en.wikipedia.org/wiki/Main_Page";
     }
-
-    private String pageURL = "http://en.wikipedia.org/wiki/Main_Page";
-
-    @FindBy(id = "p-logo")
-    private WebElement wikiLogo;
 
     @FindBy(id = "mp-topbanner")
     private WebElement topBanner;
 
     @FindBy(xpath = "//*[@id='mp-topbanner']/tbody/tr/td[1]/table/tbody/tr/td/div[1]")
     private WebElement welcomeMessage;
-
-    @FindBy(id = "searchInput")
-    private WebElement searchField;
-
-    @FindBy(id = "searchButton")
-    private WebElement searchButton;
-
-    public WebElement getWikiLogo() {
-        return wikiLogo;
-    }
 
     public WebElement getTopBanner() {
         return topBanner;
@@ -40,16 +25,4 @@ public class WikipediaMainPage extends Page {
         return welcomeMessage;
     }
 
-    public WebElement getSearchField() {
-        return searchField;
-    }
-
-    public WebElement getSearchButton() {
-        return searchButton;
-    }
-
-    @Override
-    public void openPage() {
-        driver.get(pageURL);
-    }
 }
